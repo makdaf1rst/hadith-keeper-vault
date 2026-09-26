91583cdb6bfcdc1299d8ebad651ab577cde4542f
sadman712-svg <sadman712@gmail.com>
2026-09-26 00:28:44 -0400

    Add Listen control to hadith entries

--- src/components/library/HadithView.tsx
+++ src/components/library/HadithView.tsx
@@ -3,6 +3,7 @@ import { Copy, Share2 } from "lucide-react";
 import { toast } from "sonner";
 
 import { BookmarkButton } from "@/components/library/BookmarkButton";
+import { HadithAudioPlayer } from "@/components/library/HadithAudioPlayer";
 import { Button } from "@/components/ui/button";
 import { fetchBengaliStructure, fetchBengaliTranslation } from "@/lib/bengali-translations";
 import { getBengaliBookTitle } from "@/lib/bengali-book-titles";
@@ -177,6 +178,11 @@ export function HadithView({
           >
             <Share2 /> {isBengali ? "শেয়ার" : "Share"}
           </Button>
+          <HadithAudioPlayer
+            hadith={hadith}
+            englishText={english}
+            context={context}
+          />
         </div>
         )}
       </header>
